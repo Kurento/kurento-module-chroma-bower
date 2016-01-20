47a014d3690beb10532ffc8ba5b9cc15674e87cc
jenkins <jenkins@kurento.org>
2016-01-20 15:32:26 +0000

    Generated code from kurento-module-chroma-js:90c62fe

--- js/kurento-module-chroma.js
+++ js/kurento-module-chroma.js
@@ -352,7 +352,7 @@ exports.WindowParam = WindowParam;
  */
 
 Object.defineProperty(exports, 'name',    {value: 'chroma'});
-Object.defineProperty(exports, 'version', {value: '6.2.1-dev'});
+Object.defineProperty(exports, 'version', {value: '6.3.0'});
 
 
 var ChromaFilter = require('./ChromaFilter');
